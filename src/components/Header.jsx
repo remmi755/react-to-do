870642e1461e6b121c47a46b2cc584f0e24fa4a3
remmi755 <90457721+remmi755@users.noreply.github.com>
2023-04-03 16:19:31 +0300

    add test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return(
         <header className="header">
-            <div>My tasks</div>
+            <div>My taskses</div>
         </header>
     )
 }
